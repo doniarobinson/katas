bb5c08c8b38eda2750fe1826c7d2295dcbcfe349
doniarobinson <doniamae@gmail.com>
2017-04-19 09:05:39 -0500

    feat: Added variation of FizzBuzz that prompts for user input for starting number

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -22,5 +22,45 @@ function fizzbuzz() {
     console.log(output);
   }    
 }
+
+/**
+ *
+ * Prompts user for a beginning number.  Prints the numbers inclusively to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+ *
+ */ 
+
+const readline = require('readline');
+
+function fizzbuzz_prompt() {
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  /*prompt("Enter FizzBuzz beginning number: ");
+  var beg = readline();*/
+
+  rl.question('Enter FizzBuzz beginning number: ', (beg) => {
+
+    rl.close();
+    
+    var output;
+    
+    for(var i=beg;i<=100;i++) {
+      output = "";
+      if (i%3 === 0) {
+        output += "Fizz";
+      }
+      if (i%5 === 0) {
+        output += "Buzz";
+      }
+      if (output === "") {
+        output = i;
+      }
+      console.log(output);
+    }
+  });
+}
  
- fizzbuzz();
+ fizzbuzz_prompt();
